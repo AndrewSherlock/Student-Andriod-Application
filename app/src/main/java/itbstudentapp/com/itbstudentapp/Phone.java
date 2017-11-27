288e6e322feeaf6a82a62619607e9ecb81fb8d88
kendok <kenkilmartin@gmail.com>
2017-11-27 07:52:17 +0000

    trying to add phone parts --Catharine parts included

--- app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
+++ app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
@@ -1,13 +1,49 @@
 package itbstudentapp.com.itbstudentapp;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.content.Intent;
+//import android.view.View;
+//import android.content.pm.PackageManager;
+//import android.net.Uri;
+//import android.Manifest;
+//import android.support.v4.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Phone extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
+
+        button = (Button) findViewById(R.id.buttonCall);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0857121499"));
+
+                if (ActivityCompat.checkSelfPermission(Phone.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
     }
 }
