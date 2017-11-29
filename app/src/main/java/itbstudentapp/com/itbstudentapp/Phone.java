a1c7e911d82ccfaf42e0430bae3ab3f673717683
kendok <kenkilmartin@gmail.com>
2017-11-29 23:45:59 +0000

    Got phone to put in the number it does not just call like i had wanted but is close

--- app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
+++ app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
@@ -1,14 +1,5 @@
 package itbstudentapp.com.itbstudentapp;
 
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.widget.Button;
-//import android.content.Intent;
-//import android.view.View;
-//import android.content.pm.PackageManager;
-//import android.net.Uri;
-//import android.Manifest;
-//import android.support.v4.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Intent;
@@ -20,9 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+
 public class Phone extends AppCompatActivity {
 
-    private Button button;
+    private Button securityButt;
+    private Button nurseButt;
 
 
     @Override
@@ -30,23 +23,40 @@ public class Phone extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
 
-        button = (Button) findViewById(R.id.buttonSecurity);
+        securityButt = (Button) findViewById(R.id.buttonSecurity);
+        nurseButt = (Button) findViewById(R.id.buttonNurse);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        securityButt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
-                String Security = " 018851099";
-                String Nurse ="018851105";
-                String Counsellor = "018851321";
+
+             /*   String Nurse ="018851105";
+                String Counsellor = "018851321"; */
 
 
                 Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:"+ Security));
+                callIntent.setData(Uri.parse("tel:018851099"));
+/*
+                If change to call the number straight away some issue with permissions become an issue maybe something we can look at
 
                 if (ActivityCompat.checkSelfPermission(Phone.this,
                         Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     return;
-                }
+                } */
+                startActivity(callIntent);
+            }
+        });
+        nurseButt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+
+              String Counsellor = "018851321";
+
+
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:018851105"));
+
                 startActivity(callIntent);
             }
         });
