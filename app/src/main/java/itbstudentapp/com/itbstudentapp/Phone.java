72c5f25c0252a80c6877981e55cab9a85f5afec8
kendok <kenkilmartin@gmail.com>
2017-12-02 23:21:16 +0000

    Put in a back button to go back to main

--- app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
+++ app/src/main/java/itbstudentapp/com/itbstudentapp/Phone.java
@@ -1,12 +1,10 @@
 package itbstudentapp.com.itbstudentapp;
 
 
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
